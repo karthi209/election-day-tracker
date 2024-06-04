415fab1de488dcb0fc9484c3d8111f1e7951a029
karthikeyan manimaran <karthikeyan14june@gmail.com>
2024-06-04 12:05:41 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/election-day-tracker/" // YOUR REPO NAME HERE
+  base: "/election-day-tracker/"
 })
